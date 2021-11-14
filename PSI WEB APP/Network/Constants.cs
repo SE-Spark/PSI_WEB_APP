60b1335fcdbea7b8b58ce9af268705a56d699cdc
Samuel munyeke, Software Engineer <59661548+SE-Spark@users.noreply.github.com>
2021-11-14 18:49:16 +0300

    Update Constants.cs

--- PSI WEB APP/Network/Constants.cs
+++ PSI WEB APP/Network/Constants.cs
@@ -7,7 +7,7 @@ namespace PSI_WEB_APP.Network
 {
     public static class Constants
     {
-        public const String BASE_URL = "https://7378-197-232-247-107.ngrok.io/api";
+        public const String BASE_URL = "https://firstwb.azurewebsites.net/api";
         //public const String BASE_URL = "http://localhost:8098/api";
         //specific
         public const String URL_CITIZEN = "/ct/";
